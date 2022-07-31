49df64f553bd10eb89bb4f511e9621fd847f5e0f
TungNguyen <tungnt172000@gmail.com>
2022-07-31 17:05:31 +0700

    Create HUB - Heads up display

--- Main.gd
+++ Main.gd
@@ -5,16 +5,19 @@ var score
 
 func _ready():
 	randomize()
-	new_game()
 	
 func game_over():
 	$ScoreTimer.stop()
 	$MobTimer.stop()
+	$HUD.show_game_over()
 	
 func new_game():
+	get_tree().call_group("mobs", "queue_free")
 	score = 0
 	$Player.start($StartPosition.position)
 	$StartTimer.start()
+	$HUD.update_score(score)
+	$HUD.show_message("Get ready")
 
 
 
@@ -28,6 +31,7 @@ func _on_StartTimer_timeout():
 
 func _on_ScoreTimer_timeout():
 	score += 1
+	$HUD.update_score(score)
 
 
 func _on_MobTimer_timeout():
@@ -44,3 +48,7 @@ func _on_MobTimer_timeout():
 	mob.linear_velocity = Vector2(rand_range(150.0, 250.0), 0).rotated(direction)
 
 	add_child(mob)
+
+
+func _on_HUD_start_game():
+	new_game()
